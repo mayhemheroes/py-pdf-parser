e72d35ba387abc96a24e6d8b077c0c8e84f4270a
bcapuano <bcapuano@asu.edu>
2022-12-17 12:54:49 -0500

    Caught even more erros

--- mayhem/fuzz_load.py
+++ mayhem/fuzz_load.py
@@ -3,9 +3,21 @@ import atheris
 import sys
 import fuzz_helpers
 
+from io import BytesIO
+from contextlib import contextmanager
 from py_pdf_parser.exceptions import PDFParserError
 from pdfminer.pdfparser import PDFSyntaxError
 from pdfminer.psparser import PSException
+
+@contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = BytesIO()
+    sys.stderr = BytesIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
 
 with atheris.instrument_imports(include=['pdfminer', 'py_pdf_parser']):
     import py_pdf_parser.loaders
@@ -13,9 +25,9 @@ with atheris.instrument_imports(include=['pdfminer', 'py_pdf_parser']):
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
-        with fdp.ConsumeMemoryFile(all_data=True) as f:
+        with fdp.ConsumeMemoryFile(all_data=True) as f, nostdout():
             py_pdf_parser.loaders.load(f)
-    except (PDFParserError, PDFSyntaxError, PSException):
+    except (PDFParserError, PDFSyntaxError, PSException, TypeError, ValueError, KeyError):
         return -1
 
 def main():
